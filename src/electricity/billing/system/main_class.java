6c64121ad850059dfd8cb3e958b648cbd66b2237
Lakshit-eng <lakshitgupta199@gmail.com>
2024-09-04 09:23:21 +0530

    added new menu options

--- src/electricity/billing/system/main_class.java
+++ src/electricity/billing/system/main_class.java
@@ -15,12 +15,12 @@ public class main_class extends JFrame {
        JLabel bg_image = new JLabel(image2);
        add(bg_image);
 
-                             //use menu bar in this page
+       //use menu bar in this page
 
         JMenuBar menubar = new JMenuBar();
         setJMenuBar(menubar);
 
-                                  //add items in menu bar
+        //add items in menu bar
 
         JMenu menu = new JMenu("Menu");
 
@@ -69,6 +69,75 @@ public class main_class extends JFrame {
         calculateBill.setIcon(new ImageIcon(calc2));
         menu.add(calculateBill);
 
+
+        //add new element "Information"
+
+        JMenu info = new JMenu("Information");
+        info.setFont(new Font("serif",Font.PLAIN,15));
+        menubar.add(info);
+
+        //add dropdown options
+
+        JMenuItem update = new JMenuItem("Update Information");
+        update.setFont(new Font("monospaced",Font.PLAIN,14));
+        //add image icon
+        ImageIcon update_img = new ImageIcon(ClassLoader.getSystemResource("icon/refresh.png"));
+        Image update_img2 = update_img.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        update.setIcon(new ImageIcon(update_img2));
+        info.add(update);
+
+        //view Information
+
+        JMenuItem view = new JMenuItem("View Information");
+        view.setFont(new Font("monospaced",Font.PLAIN,14));
+        //add image icon
+        ImageIcon viewImg = new ImageIcon(ClassLoader.getSystemResource("icon/information.png"));
+        Image viewImg2 = viewImg.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        view.setIcon(new ImageIcon(viewImg2));
+        info.add(view);
+
+        //add new element user
+
+        JMenu user = new JMenu("User");
+        user.setFont(new Font("serif",Font.PLAIN,15));
+        menubar.add(user);
+
+        //add dropdown items
+
+        JMenuItem paybills = new JMenuItem("Pay Bills");
+        paybills.setFont(new Font("monospaced",Font.PLAIN,14));
+
+        //add image icon
+        ImageIcon paybill = new ImageIcon(ClassLoader.getSystemResource("icon/pay.png"));
+        Image bill = paybill.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        paybills.setIcon(new ImageIcon(bill));
+        user.add(paybills);
+
+        JMenuItem billDetails = new JMenuItem("Bill Details");
+        billDetails.setFont(new Font("monospaced",Font.PLAIN,14));
+
+        //add Image icon
+        ImageIcon Details = new ImageIcon(ClassLoader.getSystemResource("icon/detail.png"));
+        Image Details_img = Details.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        billDetails.setIcon(new ImageIcon(Details_img));
+        user.add(billDetails);
+
+        //add a new element
+
+        JMenu Bills = new JMenu("Bills");
+        Bills.setFont(new Font("serif",Font.PLAIN,15));
+        menubar.add(Bills);
+
+        //add dropdown Items
+
+        JMenuItem genBill = new JMenuItem("Generate Bill");
+        genBill.setFont(new Font("monospaced",Font.PLAIN,14));
+
+        //add image icon
+        ImageIcon genbills = new ImageIcon(ClassLoader.getSystemResource("icon/bill.png"));
+        Image genbill2 = genbills.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        genBill.setIcon(new ImageIcon(genbill2));
+        Bills.add(genBill);
 
        setLayout(new FlowLayout());
        setVisible(true);
